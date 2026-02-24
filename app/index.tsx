cd6448b8ce3f3bc0bf2e0eef2cf7dbd0630e98ab
José Rangel <jose.rangel7030@gmail.com>
2026-02-24 14:08:19 -0600

    First Commit

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Hola Mundo 👋</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+  texto: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+});
